e1735b28705d151ee43534b6b9f5bd02bf2360ba
Peter-Devine <49399312+Peter-Devine@users.noreply.github.com>
2020-12-07 16:52:14 +1300

    Add 3 types of ROC AUC metric to evaluate distances

---
+++ analysis/metrics/roc_auc_cosine.py
@@ -0,0 +1,33 @@
+from scipy import spatial
+from sklearn import metrics
+import numpy as np
+
+class Metric:
+
+    # Because the labels are the same across encodings, we calculate the label similarity only once
+    def __init__(self, labels):
+        # Create a series of series which state whether the two elements at each index have any shared labels in their list of labels
+        self.similarities = labels.apply(lambda outer_list: labels.apply(lambda inner_list: not set(outer_list).isdisjoint(inner_list))).values
+
+        # If all values are True, then there are no different labels in the dataset (all points share at least one label with every other point)
+        # In this instance, we set self.is_all_true to True, and analyse.py will  as the ROC curve would then be meaningless
+        self.is_possible = not self.similarities.all().all()
+
+    # Finds the average ROC over all points based on distance, where a 1 is defined as another point with a label that overlaps with the orginal point.
+    def calculate(self, embeddings):
+        distance_metric="cosine"
+
+        # Get the distances between each embedding
+        mutual_distances = spatial.distance.squareform(spatial.distance.pdist(embeddings, metric=distance_metric))
+
+        print("cosine")
+        print(np.any(np.isnan(self.similarities)))
+        print(np.any(np.isnan(mutual_distances)))
+        # Make the nan values (distances to sentences with 0 vector sentence embeddings) 2, the maximum for cosine distance
+        mutual_distances[np.isnan(mutual_distances)] = 2
+
+        # Calculate the roc auc score based on the similarity of labels (binary "yes, the two observations share at least one label" or "no, they share no labels")
+        # The likelihood of them being classified as being similar is inversely proportional to the distance between them.
+        rocs = [metrics.roc_auc_score(sim, dis) for sim, dis in zip(self.similarities, -mutual_distances)]
+
+        return sum(rocs) / len(rocs)
