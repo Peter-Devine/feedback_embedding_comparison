37344dc1219e5c4967b9b3a8d4d6aa09a5371c81
Peter Devine <pdev438@UoA.auckland.ac.nz>
2021-01-29 21:24:52 +1300

    Add dataset name to tf hub base

--- encoding/tf_hub_encoder_base.py
+++ encoding/tf_hub_encoder_base.py
@@ -4,5 +4,5 @@ class TfHubEncoderBase:
     def __init__(self, model_url):
         self.model = hub.load(model_url)
 
-    def encode(self, text_list):
+    def encode(self, text_list, dataset_name=None):
         return self.model(text_list).numpy()
