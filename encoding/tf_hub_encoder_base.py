8ffef16ac08d44895e853d5c6020d5749b7ad43e
Peter-Devine <49399312+Peter-Devine@users.noreply.github.com>
2021-03-29 13:18:58 +1300

    Add batches to tf-hub models

--- encoding/tf_hub_encoder_base.py
+++ encoding/tf_hub_encoder_base.py
@@ -1,9 +1,14 @@
 import tensorflow_hub as hub
 import tensorflow_text
+import numpy as np
 
 class TfHubEncoderBase:
     def __init__(self, model_url):
         self.model = hub.load(model_url)
+        self.model_url = model_url
+        self.batch_size = 64
 
     def encode(self, text_list, dataset_name=None):
-        return self.model(text_list).numpy()
+        embedding = [self.model(text_list[i:i+self.batch_size]).numpy() for i in tq.tqdm(range(0, len(text_list), self.batch_size), desc=f"{self.model_url} embed")]
+        embedding = np.vstack(embedding)
+        return embedding
