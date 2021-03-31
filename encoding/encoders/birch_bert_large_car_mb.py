42f689a1535b200200c01ab2027640e7c98074a6
Peter-Devine <49399312+Peter-Devine@users.noreply.github.com>
2021-03-31 13:59:44 +1300

    Add Capreolus encoders

---
+++ encoding/encoders/birch_bert_large_car_mb.py
@@ -0,0 +1,6 @@
+from encoding.sentence_transformers_encoder_base import SentenceTransformerEncoderBase
+
+class Encoder(SentenceTransformerEncoderBase):
+    def __init__(self):
+        model_name = "Capreolus/birch-bert-large-car_mb"
+        super(Encoder, self).__init__(model_name)
